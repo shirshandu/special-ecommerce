3bfddcce2dc33cc534af0d52be3056992de7d8b8
shirshandu <shirshandu@yahoo.com>
2020-11-13 17:55:04 +0530

    updated reducers and added cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils.js";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./header.styles.scss";
 
@@ -26,7 +28,9 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    <CartDropdown/>
   </div>
 );
 
