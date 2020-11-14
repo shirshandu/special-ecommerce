bed2659f7a63c8402a99ed960f20a0cb48ba7547
shirshandu <shirshandu@yahoo.com>
2020-11-14 20:00:06 +0530

    update shop data, updated cart utils to group items with qty

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import './custom-button.styles.scss';
+import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={`${isGoogleSignIn ? 'isGoogleSignIn': '' } custom-button`} {...otherProps}>
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? "inverted" : ""} custom-button`}
+    {...otherProps}
+  >
     {children}
   </button>
 );
